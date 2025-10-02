479581dbaa2ee31170f36f24778081f5c5b2256a
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-10-02 13:53:36 -0400

    Update immediate_generator.v

--- SingleCoreProcessor/Immediate Generator/immediate_generator.v
+++ SingleCoreProcessor/Immediate Generator/immediate_generator.v
@@ -28,23 +28,25 @@ module immediate_generator (
   always @(*) begin
     case instruction[6:0]
       0010011: // I-type
-        immediate <= instruction[31:20];  
+        immediate = instruction[31:20];  
       0100011: // S-type
-        immediate <= {instruction[31:25]; instruction_reg[11:7]}; 
+        immediate = {instruction[31:25]; instruction[11:7]}; 
       1100011: // B-type
-        immediate <= {instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+        immediate = {{19{instruction[31]}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
 
       // two different types of U-type instructions
       0110111: // Load Upper Immediate
-        immediate <= instruction[31:12];
+        immediate = {instruction[31:12], 12'b0};
       0010111: // Add Upper Immediate to PC
-        immediate <= instruction[31:12];
+        immediate = instruction[31:12];
 
       // two different types of J-type instructions
       1101111: // jalr
-        immediate <= instruction[31:20];
+        immediate = instruction[31:20];
       1111111: // jal
-        immediate <= {instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+        immediate = {instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+      default:
+        immediate = 32'b0;
     endcase
   end
 
