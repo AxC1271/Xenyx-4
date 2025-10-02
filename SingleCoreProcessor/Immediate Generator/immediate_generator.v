cad8a036f00e93bebe2cd613772fbc3402e4f428
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-10-02 13:27:50 -0400

    Update immediate_generator.v

--- SingleCoreProcessor/Immediate Generator/immediate_generator.v
+++ SingleCoreProcessor/Immediate Generator/immediate_generator.v
@@ -1,1 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Andrew Chen
+// 
+// Create Date: 10/2/2025 01:25:22 PM
+// Design Name: Immediate Generator
+// Module Name: immediate_generator
+// Project Name: Xenyx-4 Multicore Processor
+// Target Devices: 
+// Tool Versions: Xilinx Vivado 2024.2
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+module immediate_generator (
+  input  [31:0]  instruction,
+  output [31:0]  immediate
+);
+
+endmodule
